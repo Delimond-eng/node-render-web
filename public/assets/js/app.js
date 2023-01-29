85405070686a000647af2690cb808a8e73e24b84
Delimond-Dev <delimonddev@localhost.localdomain>
2023-01-29 02:09:12 +0100

    update nodes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,14 @@
-window.addEventListener("DOMContentLoaded", async (event) => {});
-
-const btn = document.getElementById("btn-read-more");
-
-btn.addEventListener("click", async (e) => {
+window.addEventListener("DOMContentLoaded", async (event) => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts"
   );
   document.getElementById("axios-res").innerText = JSON.stringify(data);
+});
+
+const btn = document.getElementById("btn-read-more");
+
+/*async api call*/
+btn.addEventListener("click", async (e) => {
+  const { data } = await axios.get("/create-test");
+  console.log(data);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const { getPosts } = require("../http/posts.http");
 
 router.get("/", async (req, res) => {
-  let all = await getPosts();
-  res.render("pages/index", { title: "Accueil", posts: all.data });
+  try {
+    let all = await getPosts();
+    res.render("pages/index", { title: "Accueil", posts: all.data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.get("/about", (req, res) => {
   res.render("pages/about", { title: "A propos" });
+});
+
+router.get("/create-test", (req, res) => {
+  res.json({
+    status: "success",
+    message: "request is Done !",
+  });
 });
 
 router.get("/features", (req, res) => {
